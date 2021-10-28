faae09ec139e2e229b1bfc3ad87dc8505ac40891
GlacierPark19 <55335173+GlacierPark19@users.noreply.github.com>
2021-10-28 11:50:46 -0700

    Update cti-rail.java

--- src/main/java/com/cti/rail/cti-rail.java
+++ src/main/java/com/cti/rail/cti-rail.java
@@ -1,2 +1,79 @@
 package com.cti.rail
 
+
+
+
+@Mod("rail")
+public class rail {
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "rail";
+
+    public rail() {
+
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+        BlockHandlerRail.init();
+        // copper_block.init();
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, LightonianFurnaceServerConfig.SERVER_CONFIG);
+        LightonianFurnaceServerConfig.loadConfig(LightonianFurnaceServerConfig.SERVER_CONFIG, FMLPaths.GAMEDIR.get().resolve(FMLConfig.defaultConfigPath()).resolve(MODID + "-server.toml"));
+    }
+
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+        // some example code to dispatch IMC to another mod
+        InterModComms.sendTo("universeawakening", "helloworld", () -> {
+            LOGGER.info("Hello world from the MDK");
+            return "Hello world";
+        });
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+        // some example code to receive and process InterModComms from other mods
+        LOGGER.info("Got IMC {}", event.getIMCStream().
+                map(m -> m.getMessageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        // do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+
+    public static final ItemGroup TAB = new ItemGroup("CTIRail") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.COPPER_INGOT.get());
+        }
+    };
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // register a new block here
+            LOGGER.info("HELLO from Register Block");
+        }
+    }
+
+
+
+}
